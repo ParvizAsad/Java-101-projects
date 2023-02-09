8348f2f0484ba0a764d6a24a976e2514260ee3a0
ParvizAsad <86598426+ParvizAsad@users.noreply.github.com>
2023-02-09 13:48:27 +0400

    Native Math.Pow DONE

--- Loop/Loop-Practice/src/NativePow.java
+++ Loop/Loop-Practice/src/NativePow.java
@@ -1,25 +1,30 @@
 public class NativePow {
     public static void main(String[] args) {
+        System.out.println(Math.pow(3,3));
         int base = Util.intInput("Hansi ədədi yoxlamaq istəyirsiniz?");
         int exponent = Util.intInput("Yoxlamaq istədiyiniz  qüvvəti daxil edin:");
         System.out.println(base + " ədədi = "+ exponent+ " üstü "+nativePow(base,exponent) +  "."  );
     }
 
     static int nativePow(int base,int exponent ){
-        int mod = 0;
-        int tempNum;
-        boolean continues = true;
-        int count = 0;
-        while (continues) {
-            mod = base % exponent;
-            if (mod == 0) {
-                count++;
-                base = base/exponent;
-            } else if (mod!=0) {
-                continues=false;
-            }
+//        int mod = 0;
+//        int tempNum;
+//        boolean continues = true;
+//        int count = 0;
+//        while (continues) {
+//            mod = base % exponent;
+//            if (mod == 0) {
+//                count++;
+//                base = base/exponent;
+//            } else if (mod!=0) {
+//                continues=false;
+//            }
+//        }
+int tempData =1;
+        for (int i=1; i<=exponent; i++){
+            tempData*=base;
         }
 
-        return count;
+        return tempData;
     }
 }
