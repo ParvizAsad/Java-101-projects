a9e42055da772298cfae2c94b1af190e3660dede
ParvizAsad <86598426+ParvizAsad@users.noreply.github.com>
2023-02-09 13:38:53 +0400

    Kombinasyon DONE

---
+++ Loop/Loop-Practice/src/Kombinasyon.java
@@ -0,0 +1,34 @@
+public class Kombinasyon {
+//    N elemanlı bir kümenin elemanları ile oluşturulacak r elemanlı farklı
+//    grupların sayısı n’in r’li kombinasyonu olarak adlandırılır. N’in r’li kombinasyonu
+//    C(n,r) şeklinde gösterilir.
+//
+//    Java ile kombinasyon hesaplayan program yazınız.
+//
+//    Kombinasyon formülü
+//    C(n,r) = n! / (r! * (n-r)!)
+    public static void main(String[] args) {
+
+        int r = Util.intInput("r: ");
+        int n = Util.intInput("n: ");
+        int c=0;
+        if (n>r){
+            c=faktoryal(n)/(faktoryal(r)*faktoryal(n-r));
+            System.out.println("C: "+c);
+        }
+        else {
+            System.out.println("n r-dən böyük olmalıdır!");
+        }
+
+
+
+    }
+
+    static int faktoryal(int a){
+        int multiplication=1;
+        for (int i =1; i<=a;i++){
+            multiplication*=i;
+        }
+        return multiplication;
+    }
+}
