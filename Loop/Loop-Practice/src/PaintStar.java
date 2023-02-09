6b2e8733850c1642993c6118a0a92123c644d8d8
ParvizAsad <86598426+ParvizAsad@users.noreply.github.com>
2023-02-09 18:23:01 +0400

    PaintStar DONE

---
+++ Loop/Loop-Practice/src/PaintStar.java
@@ -0,0 +1,28 @@
+public class PaintStar {
+    public static void main(String[] args) {
+        int n = Util.intInput("Sayi daxil edin: ");
+int temp;
+int mod=0;
+
+        for (int i = 0; i <= n ; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+        for (int i = n; i >= 1 ; i--) {
+            for (int j = (n - i); j >0 ; j--) {
+                System.out.print(" ");
+            }
+            for (int k = (2 * i + 1); k >=1 ; k--) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
