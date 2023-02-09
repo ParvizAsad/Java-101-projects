34bedd8eef76ed2b29baf8ec9d9097c9f3f01192
ParvizAsad <86598426+ParvizAsad@users.noreply.github.com>
2023-02-09 14:01:11 +0400

    task finshed

---
+++ Loop/Loop-Practice/src/Armstrong.java
@@ -0,0 +1,21 @@
+public class Armstrong {
+    //    Bir sayının basamak sayılarının toplamını hesaplayan program yazınız.
+    public static void main(String[] args) {
+        int a = Util.intInput("Ədədi daxil edin: ");
+        boolean check = true;
+        int multip;
+        int mod;
+        int sum = 0;
+        while (check) {
+            if (a / 10 != 0) {
+                sum += a % 10;
+               a = a / 10;
+            } else {
+                sum += a % 10;
+                check = false;
+            }
+        }
+        System.out.println(sum);
+
+    }
+}
