fbfd1b9dc223354309b76fdea87257b7026c50fd
ParvizAsad <86598426+ParvizAsad@users.noreply.github.com>
2023-02-10 22:58:53 +0400

    Recursive Fibonacci DONE

---
+++ Metodlar/Methods-Practise/src/RecursiveFibonacci.java
@@ -0,0 +1,21 @@
+public class RecursiveFibonacci {
+
+    public static void main(String[] args) {
+        int n = Util.intInput("Element sayini daxil edin: ");
+
+        System.out.println(recursiveMethod(n));
+
+
+    }
+
+    static int recursiveMethod(int n){
+if (n==1 || n==2){
+    return 1;
+}
+return recursiveMethod(n-1)+recursiveMethod(n-2);
+
+
+    }
+
+
+}
